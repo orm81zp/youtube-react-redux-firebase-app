b5ac87a656b685005f85b71ea80422a7397811e1
Roman Onishchenko <roman@Romans-MacBook-Pro.local>
2020-02-01 16:49:22 +0200

    navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
 
 const Navbar = () => {
     return (
         <nav className="nav-wrapper grey darken-3">
             <div className="container">
                 <Link className="brand-logo" to="/">MarioPlan</Link>
+                <SignedInLinks />
+                <SignedOutLinks />
             </div>
         </nav>
     )
